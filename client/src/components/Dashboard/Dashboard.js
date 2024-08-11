07666ecbdb8aa662a5d32aa983246d423fd7603c
Arbaz khan <Arbaz151033@gmail.com>
2024-08-11 13:44:13 +0530

    s

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DNavbar from "./DNavbar";
 import { Outlet, Route, Routes } from "react-router-dom";
+import "./Dashboard.css";
+import axios from "axios";
 
 function Dashboard() {
+  const [data, setData] = useState()
+  const date = new Date();
+  let datee = (date.getDate()) 
+  let month = date.getMonth() 
+  let year = date.getFullYear() 
+
+  let todayDate = `${year}-0${month}-${datee}`;
+  console.log(todayDate)
+
+  function getData(){
+  axios.get(`https://lucky-shop-backend.onrender.com/sales/${todayDate}`).then((resp)=>{
+    setData(resp.data.date)
+    console.log(data)
+  }).catch((error)=>{
+    console.log(error)
+  })}
+
+  useEffect(() => {
+    
+  getData();
+  }, [])
+  
   return (
     <>
-    <DNavbar/>
-    <Outlet />
-    
-      
+      <DNavbar />
+      <Outlet />
+
+      <div className="rightBar">
+        <div className="card" >
+          <div className="card-body ">
+            <p className="card-text">Today's Sales :</p>
+            <p className="card-text">Today's Total :</p>
+          </div>
+        </div>
+      </div>
+
     </>
   );
 }
